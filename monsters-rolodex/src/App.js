7812e80650031f85bfda4f99a347bd8fe4f4722b
Jaino Vennatt <jainovennatt@Jainos-MacBook-Pro.local>
2020-01-20 14:16:32 -0600

    backup push

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters: [
+        {
+          name: 'Ender Man',
+          id: 'mn0'
+        },
+        {
+          name: 'Spider',
+          id: 'mn1'
+        },
+        {
+          name: 'Blaze',
+          id: 'mn2'
+        },
+        {
+          name: 'Creeper',
+          id: 'mn3'
+        },
+        {
+          name: 'Ghast',
+          id: 'mn4'
+        },
+        {
+          name: 'Skeleton',
+          id:  'mn5'
+        },
+        {
+          name: 'Witch',
+          id:  'mn6'
+        },
+        {
+          name: 'Slime',
+          id:  'mn7'
+        }
+      ]
+    };
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map( monster => <h1 key={monster.id}>{monster.name} </h1>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
